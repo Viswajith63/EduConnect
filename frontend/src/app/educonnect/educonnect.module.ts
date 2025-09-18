484153f332c8822c3ec90f5de2e4490504210da1
username <wecp-dummy@gmail.com>
2025-09-18 18:26:27 +0000

    day20

--- frontend/src/app/educonnect/educonnect.module.ts
+++ frontend/src/app/educonnect/educonnect.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from "@angular/common";
 import { EduConnectRoutingModule } from "./educonnect-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { EnrollmentComponent } from "./components/enrollment/enrollment.component";
+import { StudentCreateComponent } from "./components/studentcreate/studentcreate.component";
+import { TeacherCreateComponent } from "./components/teachercreate/teachercreate.component";
+import { CourseCreateComponent } from "./components/coursecreate/coursecreate.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EnrollmentComponent,
+    StudentCreateComponent,
+    TeacherCreateComponent,
+    CourseCreateComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
   exports: [
-    
+
   ]
 })
 export class EduconnectModule {}
